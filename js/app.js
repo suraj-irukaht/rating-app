fb1c2a5c0ef9ae1ca9e565e3893351e853db57d1
Suraj Shahi <suraj.shahithakuri@md.p2h.com>
2023-03-25 16:34:18 +0545

    changed js

--- js/app.js
+++ js/app.js
@@ -6,35 +6,21 @@ const intiSubmit = () => {
    // selection all element
    const form = document.querySelector('.form');
    const currentRating = document.querySelector('.current-rating');
-   const OutOf_Rating = document.querySelector('.out-of');
    const input = document.querySelectorAll('input[type="radio"]');
-
-   input.forEach((el) => {
-      let inputValue = el.value;
-      let span = el.nextElementSibling;
-      span.innerHTML = inputValue;
-      OutOf_Rating.innerHTML = input.length;
-   });
+   const card = document.querySelector('.card');
 
    form.addEventListener('submit', (e) => {
       e.preventDefault();
-      let preloader = document.querySelector('.preloader');
-      let cardResponse = document.querySelector('.card-response');
-      let defaultCard = document.querySelector('.default-card');
-
-      preloader.style.display = 'block';
-      setTimeout(() => {
-         let selectedValue;
-         input.forEach((el) => {
-            if (el.checked) {
-               selectedValue = el.value;
-            }
-         });
-
-         preloader.style.display = 'none';
-         defaultCard.style.display = 'none';
-         cardResponse.style.display = 'block';
-         currentRating.innerHTML = selectedValue;
-      }, 5000);
+      let selectedValue;
+      input.forEach((el) => {
+         if (!el.checked) {
+            card.classList.add('error');
+         } else if (el.checked) {
+            selectedValue = el.value;
+            card.classList.add('active');
+            card.classList.remove('error');
+         }
+      });
+      currentRating.innerHTML = selectedValue;
    });
 };
